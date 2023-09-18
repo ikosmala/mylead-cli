620abf1c6cc5200f9a6686fb5d1ad1059afa56c8
Iwo <kosmala.iwo@gmail.com>
2023-09-18 12:33:56 +0200

    changes to tables

--- tables.py
+++ tables.py
@@ -6,9 +6,6 @@ from rich.panel import Panel
 from rich.prompt import Prompt
 from rich.table import Table
 
-# ml_subs
-# daily average, best day,
-
 
 def aggregate_data(
     data: pd.DataFrame, group_by_column: str, sort_by: str = "total_payout"
@@ -32,7 +29,7 @@ def create_table(
     caption: str,
     column_name: str,
     group_by_column: str,
-    sum_of_all_leads: int,
+    num_of_leads: int,
     sum_payouts: float,
 ) -> None:
     table = Table(title=title, caption=caption, box=box.ROUNDED, header_style="gold1")
@@ -46,7 +43,7 @@ def create_table(
     data[group_by_column] = data[group_by_column].astype(str)
 
     for _, row in data.iterrows():
-        grouped_data_percent = row["grouped_data"] / sum_of_all_leads * 100
+        grouped_data_percent = row["grouped_data"] / num_of_leads * 100
         total_payout_percent = row["total_payout"] / sum_payouts * 100
         table.add_row(
             row[group_by_column],
@@ -66,12 +63,12 @@ def table_from_data(
     column_name: str,
     sort_by: str = "total_payout",
 ) -> None:
-    start_date = data["created_at.date"].min()
-    end_date = data["created_at.date"].max()
-    sum_of_all_leads = len(data)
+    start_date = data["created_at.date"].min().date()
+    end_date = data["created_at.date"].max().date()
+    num_of_leads = len(data)
     result = aggregate_data(data, group_by_column=group_by_column, sort_by=sort_by)
     sum_payouts = result["total_payout"].sum()
-    caption = f"Data gathered between {start_date} -- {end_date}"
+    caption = f"Data gathered between {start_date} and {end_date}"
 
     create_table(
         data=result,
@@ -79,32 +76,44 @@ def table_from_data(
         caption=caption,
         column_name=column_name,
         group_by_column=group_by_column,
-        sum_of_all_leads=sum_of_all_leads,
+        num_of_leads=num_of_leads,
         sum_payouts=sum_payouts,
     )
 
 
 def choose_table(df: pd.DataFrame) -> None:
     console = Console()
-    OPTIONS = OPTIONS = {
-        "1": (
-            "Type of device on which leads were created.",
-            "user_agent.device",
-            "Device Type",
-        ),
-        "2": (
-            "Operating system on which leads were created.",
-            "user_agent.operation_system",
-            "Operating System",
-        ),
-        "3": ("Country from which leads were created.", "country", "Country"),
-        "4": ("Statistics by campaign.", "campaign_name", "Campaign Statistics"),
-        "5": ("Statistics by hour of the day.", "hour_of_day", "Hourly Statistics"),
+    OPTIONS = {
+        "1": {
+            "title": "Type of device on which leads were created.",
+            "group_by_column": "user_agent.device",
+            "column_name": "Device Type",
+        },
+        "2": {
+            "title": "Operating system on which leads were created.",
+            "group_by_column": "user_agent.operation_system",
+            "column_name": "Operating System",
+        },
+        "3": {
+            "title": "Country from which leads were created.",
+            "group_by_column": "country",
+            "column_name": "Country",
+        },
+        "4": {
+            "title": "Statistics by campaign.",
+            "group_by_column": "campaign_name",
+            "column_name": "Campaign name",
+        },
+        "5": {
+            "title": "Statistics by hour of the day.",
+            "group_by_column": "hour_of_day",
+            "column_name": "Hour",
+        },
     }
 
     console.print(
         Panel(
-            "\n".join([f"{key}. {value[0]}" for key, value in OPTIONS.items()])
+            "\n".join([f"{key}. {value['title']}" for key, value in OPTIONS.items()])
             + "\n\n[bold]0. Exit[/bold]",
             title="Available statistics",
             expand=False,
@@ -113,25 +122,16 @@ def choose_table(df: pd.DataFrame) -> None:
         )
     )
     while True:
-        table_choice = Prompt.ask(
+        choice = Prompt.ask(
             "Pick a statistic to display or exit the program.",
             choices=[*list(OPTIONS.keys()), "0"],
         )
 
-        if table_choice == "0":
+        if choice == "0":
             console.print("Exiting program.")
             break
 
-        title, group_by_column, column_name = OPTIONS.get(
-            table_choice, (None, None, None)
-        )
-
-        if title:
-            table_from_data(
-                data=df,
-                title=title,
-                group_by_column=group_by_column,
-                column_name=column_name,
-            )
+        if option := OPTIONS.get(choice):
+            table_from_data(df, **option)
         else:
             console.print("Wrong input")
